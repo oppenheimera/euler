c700ecfe9d7a00cfbd1432d86832fce44f3d6805
Ari Oppenheimer <oppenheimer@berkeley.edu>
2017-05-18 12:43:32 -0700

    added 31

--- 31_coin_sums.py
+++ 31_coin_sums.py
@@ -1,18 +1,22 @@
 """1p, 2p, 5p, 10p, 20p, 50p, £1 (100p) and £2 (200p)"""
 import time
+import sys
+
+target = int(sys.argv[-1])
 
 start = time.time()
 
-def c_d(targ):
-    """this doesn't work for targ=200"""
-    if targ == 0:
-        return 1
-    if targ < 0:
-        return 0
-    else:
-        return c_d(targ - 200) + c_d(targ - 100) + c_d(targ - 50) + c_d(targ - 20) + c_d(targ - 10) + c_d(targ - 10) + c_d(targ - 5) + c_d(targ - 2) + c_d(targ - 1)
+def count(S, m, n):
+    table = [0 for k in range(n + 1)]
+    table[0] = 1
+    for i in range(0, m):
+        for j in range(S[i], n + 1):
+            table[j] += table[j - S[i]]
+    return table[n]
 
-# ans = cd(200)
+denoms = (200,100,50,20,10,5,2,1)
+ans = count(denoms, len(denoms), target)
+
 elapsed = time.time() - start
 
 print("Found {} in  {} seconds.".format(ans, elapsed))
