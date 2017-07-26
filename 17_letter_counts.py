fbcd37986db4596c9596d4fc86451d1c4c6cf673
Ari Oppenheimer <oppenheimer@berkeley.edu>
2017-07-26 11:45:11 -0700

    A FUCKING SPELLING ERROR THIS WHOLE GDM TIME R U SERIOUS

--- 17_letter_counts.py
+++ 17_letter_counts.py
@@ -1,9 +1,12 @@
-import traceback
-
+import sys
 """
 If all the numbers from 1 to 1000 (one thousand) inclusive 
 were written out in words, how many letters would be used?
->>> nums_two([str(x) for x in range(1,1001)])
+NOTE: Do not count spaces or hyphens. For example, 
+342 (three hundred and forty-two) contains 23 letters and 
+115 (one hundred and fifteen) contains 20 letters. 
+The use of "and" when writing out numbers is in compliance 
+with British usage.
 """
 
 
@@ -16,49 +19,41 @@ do_build = {'0': 0,'2':len('twenty'), '3':len('thirty'), '4':len('forty'), '5':l
     '6':len('sixty'), '7':len('seventy'), '8':len('eighty'), '9':len('ninety')}
 triples = {'1':len('onehundred'), '2':len('twohundred'), '3':len('threehundred'), 
     '4':len('fourhundred'), '5':len('fivehundred'), '6':len('sixhundred'), 
-    '7':len('sevenhunred'), '8':len('eighthundred'), '9':len('ninehundred')}
+    '7':len('sevenhundred'), '8':len('eighthundred'), '9':len('ninehundred')}
 quads = {'1000': len('onethousand')}
 
-def number():
-    count = 0
-    for a in singles.values():
-        count += a * 100
-    for b in doubles.values():
-        count += b * 10
-    for c in do_build.values():
-        count += c * 100
-    for d in triples.values():
-        count += d * 100
-    for e in quads.values():
-        count += e
-    return count
+def get_sum(NUM):
+    N = str(NUM)
+    if NUM < 100:
+        if NUM < 10:
+            return singles[N]
+        elif NUM < 20:
+            return doubles[N]
+        return do_build[N[0]] + singles[N[1]]
+    elif NUM < 1000:
+        if NUM % 100 == 0:
+            return triples[N[0]]
+        elif NUM % 100 < 10:
+            return triples[N[0]] + len("and") + singles[N[2]]
+        elif NUM % 100 < 20:
+            return triples[N[0]] + len("and") + doubles[N[1:]]
+        return triples[N[0]] + len('and') + do_build[N[1]] + singles[N[2]]
+    elif NUM == 1000:
+        return quads[N]
 
-def nums_two(lst):
-    count = 0
-    try:
-        for x in lst:
-            if len(x) == 1:
-                count += singles[x]
-            if len(x) == 2:
-                if x in doubles:
-                    count += doubles[x]
-                else:
-                    count += singles[x[1]]
-                    count += do_build[x[0]]
-            if len(x) == 3:
-                if x[1:] != '00':
-                    count += 3
-                if x[1:] in doubles:
-                    count += doubles[x[1:]]
-                    count += triples[x[0]]
-                else:
-                    count += singles[x[2]]
-                    count += do_build[x[1]]
-                    count += triples[x[0]]
-            if len(x) == 4:
-                count += quads[x]
-    except KeyError as k:
-        traceback.print_exc()
-        print("errored out on: " + str(x))
-    print("counted " + str(count) + " letters")
-    return
+def test(S):
+    return len(S.replace(" ", ""))
+
+#test cases
+if len(sys.argv) >= 2:
+    if sys.argv[-1].lower() == 'debug':
+        assert get_sum(342) == 23
+        assert get_sum(115) == 20
+        assert sum([get_sum(n) for n in range(1,6)]) == 19
+        assert get_sum(301) == test("three hundred and one")
+        assert get_sum(111) == test("one hundred and eleven")
+        assert get_sum(65) == test("sixty five")
+        assert get_sum(555) == test("five hundred and fifty five")
+else:        
+    ANSWER = sum([get_sum(n) for n in range(1, 1001)])
+    print("Answer is {}".format(ANSWER))
