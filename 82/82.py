5b6692db225c1ab093c209a4ccaacac25fd168fb
Ari Oppenheimer <oppenheimer@berkeley.edu>
2017-10-06 11:19:39 -0700

    gonna have to use dijkstras

--- 82/82.py
+++ 82/82.py
@@ -13,22 +13,21 @@ def read_matrix(path='matrix.txt'):
 
 def get_path(M):
     A = matrix(M.size)
-    for i in range(M.size):
+    for i in range(M.size): # set A_{i,j} = infinity
         for j in range(M.size):
             A.put(i, j, sys.maxsize)
-    for i in range(M.size): # set up A_{0,i}
+    for i in range(M.size): # set A_{0,0...n-1} to be M_{i,j}
         A.put(0, i, M.at(0, i))
-    s = 0
-    A.pprint()
+
     for i in range(1, M.size): # do the work
         for j in range(M.size):
-            if i == 0:
-                global_min = min(A.at()) 
-            if i == m.size - 1:
-                global_min = 
+            if j == 0:
+                global_min = M.at(i, j) + min(A.at(i-1, j), A.at(i, j+1)) 
+            if j == M.size - 1:
+                global_min = M.at(i, j) + min(A.at(i-1, j), A.at(i, j - 1))
             else:
-                global_min = min(A.at(i, j-1), A.at(i, j+1))
-            A.put(i, j, global_min + M.at(i, j) + A.at(i-1, j)) 
+                global_min = M.at(i, j) + min(A.at(i, j-1), A.at(i, j+1), A.at(i-1, j))
+            A.put(i, j, global_min) 
     A.pprint()
     return A.at(A.size-1, A.size-1)
 
